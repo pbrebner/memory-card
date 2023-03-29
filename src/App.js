54f0675eb7df837333106687322754595ebec0d5
Patrick Brebner <patrick.brebner@gmail.com>
2023-03-29 12:43:37 -0700

    Add header and main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
 function App() {
-    return <div className="App"></div>;
+    const [score, setScore] = useState(0);
+    const [bestScore, setBestScore] = useState(0);
+
+    return (
+        <div className="App">
+            <header className="header">
+                <Header score bestScore />
+            </header>
+            <main className="main">
+                <Main />
+            </main>
+        </div>
+    );
 }
 
 export default App;
