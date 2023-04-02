420c5d4a355de689bc6ee65b8ce6a135a60ac7fa
Patrick Brebner <patrick.brebner@gmail.com>
2023-04-02 15:12:46 -0700

    Update styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "../styles/Card.css";
 
 function Card(props) {
     return (
         <div className="card" onClick={props.handleCardClick}>
             <div className="cardContent">
-                <img src={props.pokemon.image} alt={props.pokemon.name} />
-                <p>{props.pokemon.name}</p>
+                <img
+                    src={props.pokemon.image}
+                    alt={props.pokemon.name}
+                    className="cardImage"
+                />
+                <p className="cardName">{props.pokemon.name}</p>
             </div>
         </div>
     );
