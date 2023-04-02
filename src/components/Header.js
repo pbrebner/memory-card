4ef8e6c5ca3cdfa62b66bdfd5e17c770fe68c878
Patrick Brebner <patrick.brebner@gmail.com>
2023-04-02 15:11:43 -0700

    Add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "../styles/Header.css";
+import pokeball from "../assets/pokeball.png";
 
 function Header(props) {
     return (
         <div className="headerInnerWrapper">
-            <h1 className="gameName">Memory Match</h1>
+            <div className="nameContainer">
+                <img src={pokeball} alt="Pokeball" className="pokeballLogo" />
+                <h1 className="gameName">PokeMemory</h1>
+            </div>
             <div className="scores">
                 <p className="score">Score: {props.score}</p>
                 <div className="vl"></div>
