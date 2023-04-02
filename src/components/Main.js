f7b5a1b7b6efceef9a7ac81af4dbcbe486b2cc1b
Patrick Brebner <patrick.brebner@gmail.com>
2023-04-01 19:37:30 -0700

    Add api fetch data and functions to handle click

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,13 @@
 import React, { useEffect, useState } from "react";
-import uniqid from "uniqid";
 import "../styles/Main.css";
-import Card from "./Card";
+import CardGrid from "./CardGrid";
 
 function Main(props) {
-    let [gameOver, setGameOver] = useState(false);
-    let [cardList, setCardList] = useState([]);
+    const numPokemon = 12;
+    const [pokemons, setPokemons] = useState([]);
+    const [clickedPokemons, setClickedPokemons] = useState([]);
 
-    const cardInfoList = [
-        { id: 1, value: 1 },
-        { id: 2, value: 2 },
-        { id: 3, value: 3 },
-        { id: 4, value: 4 },
-        { id: 5, value: 5 },
-        { id: 6, value: 6 },
-    ];
-
+    //Utility functions: shuffle and capitalizeFirstLetter
     function shuffle(array) {
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -27,34 +19,67 @@ function Main(props) {
         return array;
     }
 
-    //Need to generate a deck of cards when the component Mounts
-    useEffect(() => {
-        let componentCardList = cardInfoList.map((cardInfo) => (
-            <Card
-                key={cardInfo.id}
-                value={cardInfo.value}
-                sortCards={useSortCards}
-            />
-        ));
-        setCardList((cardList = componentCardList));
-    }, []);
-
-    function useSortCards() {
-        console.log("Running");
-        let sortedList = shuffle(cardList);
-        setCardList(sortedList);
-        console.log(cardList);
+    function capitalizeFirstLetter(string) {
+        return string.slice(0, 1).toUpperCase() + string.slice(1);
     }
 
-    //After clicking a card, I need to check if the card was already chosen
+    //When component mounts, fetch data from API and set pokemons state
+    useEffect(() => {
+        async function loadCards() {
+            setPokemons(shuffle(await fetchPokemons(numPokemon)));
+        }
 
-    //If not already chosen, randomly sort to card list, update score and display set
+        loadCards();
+    }, []);
 
-    //If already chosen, end game, reset with new deck, and update Bestscore if necessary
+    async function fetchPokemons(amountPokemon) {
+        const pokemons = [];
+
+        for (let i = 1; i <= amountPokemon; i++) {
+            const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${i}`;
+            const response = await fetch(pokemonUrl);
+            const pokemon = await response.json();
+            const id = pokemon.id;
+            const name = capitalizeFirstLetter(pokemon.name);
+            const image = pokemon.sprites.front_default;
+            pokemons.push({ id, name, image });
+        }
+
+        return pokemons;
+    }
+
+    //When card is clicked, check clicked value against clickedPokemon state
+    function handleCardClick(e) {
+        console.log(e.target.parentNode.lastChild.textContent);
+        const pokemonName = e.target.parentNode.lastChild.textContent;
+        checkSelection(pokemonName);
+        setPokemons(shuffle(pokemons));
+    }
+
+    function checkSelection(pokemonName) {
+        if (clickedPokemons.includes(pokemonName)) {
+            resetGame();
+        } else {
+            props.incrementScore();
+            setClickedPokemons((prevState) => [...prevState, pokemonName]);
+        }
+    }
+
+    function resetGame() {
+        setClickedPokemons([]);
+        props.resetScore();
+    }
 
     return (
         <div className="mainInnerWrapper">
-            <div className="gameCards">{cardList}</div>
+            {pokemons.length ? (
+                <CardGrid
+                    pokemons={pokemons}
+                    handleCardClick={handleCardClick}
+                />
+            ) : (
+                <div></div>
+            )}
         </div>
     );
 }
